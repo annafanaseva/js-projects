b9472127b34f71e780fa0000b13e284efe142f11
Анна Афанасьева <ann.afanaseva777@gmail.com>
2022-11-08 20:01:22 +0300

    added grocery list

--- index.js
+++ index.js
@@ -23,21 +23,3 @@ function slideImg(i) {
 }
 
 slideImg(index);
-
-let weekdays = [
-  'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'
-]
-
-setInterval(function getCurrentDate() {
-  currentTime = new Date();
-  hours = currentTime.getHours();
-  minutes = currentTime.getMinutes();
-  hours = (hours < 10) ? '0' + hours : hours;
-  minutes = (minutes < 10) ? '0' + minutes : minutes;
-
-  document.getElementById("weekday").innerHTML = weekdays[currentTime.getDay()];
-  document.getElementById("hours").innerHTML = hours;
-  document.getElementById("minutes").innerHTML = minutes;
-}, 200)
-
-
