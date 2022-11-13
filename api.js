b3e654820213ea9b8ccf4940ed9f81dda71d9cd4
Анна Афанасьева <ann.afanaseva777@gmail.com>
2022-11-13 12:06:03 +0300

    fix

--- api.js
+++ api.js
@@ -1,14 +1,15 @@
 function findPictures() {
+  document.querySelector('.pictures-wrapper').replaceChildren();
+
   fetch('https://api.unsplash.com/search/photos?query=' + input.value + '&per_page=30&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo')
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
       for (let i = 0; i <= 20; i++) {
         let image = document.createElement('img');
         image.src = data.results[i].urls.full;
-        image.classList.add( 'picture' );
+        image.classList.add('picture');
         document.querySelector('.pictures-wrapper').appendChild(image);
       }
     });
